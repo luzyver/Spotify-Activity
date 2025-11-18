787e74b0692e35eda7ddc981234dd1879f7da3c2
𝙻𝚄𝚉𝚈𝚅𝙴𝚁 <rezivayra2010@gmail.com>
2025-11-18 16:15:52 +0700

    fix

--- worker/src/config/constants.js
+++ worker/src/config/constants.js
@@ -1,10 +1,5 @@
 export const CORS_HEADERS = {
 	'Access-Control-Allow-Origin': '*',
 	'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-	'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Clear-Password, X-Backup-Password',
-};
-
-export const CRON_SCHEDULES = {
-	CLEAR_HISTORY_HOUR: 16,
-	CLEAR_HISTORY_MINUTE: 59,
+	'Access-Control-Allow-Headers': 'Content-Type, Authorization',
 };
